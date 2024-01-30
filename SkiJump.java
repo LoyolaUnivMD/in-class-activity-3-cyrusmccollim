14fd9f9a20bca797a9ca101185fdec36b45fe8ae
cyrusmccollim <106127824+cyrusmccollim@users.noreply.github.com>
2024-01-30 09:55:20 -0500

    Update SkiJump.java

--- SkiJump.java
+++ SkiJump.java
@@ -4,11 +4,18 @@ import java.util.*;
 class SkiJump {
     public static void main(String[] args) {
         Scanner input = new Scanner();
+        
         System.out.println("What is the type of the hill? (Normal/Large)");
         hillType = input.nextLine();
-        Hill hill = new Hill(hillType);
+        
+        Hill hill1 = new Hill(hillType);
+        
         System.out.println("What is the jumper's speed at the end of the ramp?");
-        speed = input.nextDouble();
+        double jumperSpeed = input.nextDouble();
+
+        hill1.calculatePoints(jumperSpeed);
+
+        System.out.println(hill1.findResult()); 
     }
 
 }
