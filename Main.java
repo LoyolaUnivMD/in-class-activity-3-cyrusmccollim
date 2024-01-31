e27a4e7d4cc5a48e676c4b8be657c061a30edf3d
cyrusmccollim <106127824+cyrusmccollim@users.noreply.github.com>
2024-01-31 10:40:44 -0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -4,16 +4,41 @@ public class Main {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         
-        System.out.println("Enter the hill details. \n(Format: name height pointsPerMeter par)");
-        String[] hillDetails = input.nextLine().split(" ");
+        System.out.println("Enter the hill details. \n(Format: height pointsPerMeter par)");
+        
+        String[] hillDetailsString = inputHillDetails();
+        double[] hillDetails = convertHillDetailsDouble(hillDetailsString);
         Hill hill = new Hill(hillDetails);
 
         System.out.print("Enter the jumper's speed: ");
         double jumpSpeed = input.nextDouble();
         
         SkiJump skiJumper = new SkiJump(hill, jumpSpeed);
-
+        
         System.out.println(skiJumper.getResult());
+    }
+
+    public String[] inputHillDetails(){
+        String[] hillDetailsInput = input.nextLine().split(" ");
+        
+        while (hillDetailsInput.length() != 3){
+            System.out.println("An error occured, please try again.");
+            hillDetailsInput = input.nextLine().split(" ");
+        }
+        
+        return hillDetailsInput;
+    }
+
+    public double[] convertHillDetailsDouble(String[] hillDetailsString){
+        double[] hillDetails;
+        
+        try {
+            hillDetails = new double[]{(double) hillDetailsString[0], (double) hillDetailsString[1], (double) hillDetailsString[2]};
+        } catch (Exception e) {
+            System.out.println("An error occured, hill details not initialized.");
+        }
+        
+        return hillDetails; 
     }
 }
 
