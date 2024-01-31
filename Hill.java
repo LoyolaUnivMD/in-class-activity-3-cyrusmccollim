1fc832af0e03d6c9c03fdd3a0df85d2cc88a3000
cyrusmccollim <106127824+cyrusmccollim@users.noreply.github.com>
2024-01-31 10:05:03 -0500

    Update Hill.java

--- Hill.java
+++ Hill.java
@@ -2,11 +2,11 @@ public class Hill {
   private String name;
   private double height, pointsPerMeter, par;
 
-  public Hill(String name, double height, double pointsPerMeter, double par){
-    this.name = name;
-    this.height = height;
-    this.pointsPerMeter = pointsPerMeter;
-    this.par = par;
+  public Hill(List<Object> hillDetails){
+    name = (String) hillDetails.get(0);
+    height = (double) hillDetails.get(1);
+    pointsPerMeter = (double) hillDetails.get(2);
+    par = (double) hillDetails.get(3);
   }
 
   public String getName(){
