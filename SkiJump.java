2fadbfe91343e73c493539389e03536eb6a032fa
cyrusmccollim <106127824+cyrusmccollim@users.noreply.github.com>
2024-01-31 10:47:41 -0500

    Update SkiJump.java

--- SkiJump.java
+++ SkiJump.java
@@ -1,33 +1,20 @@
 import java.util.*;
 
 public class SkiJump {
-  private final HashMap<String, double[]> HILL_TYPES = new HashMap<>();
-  private final String hillType;
+  private final Hill hill;
   private final double jumpSpeed;
 
-  public SkiJump(String hillType, double jumpSpeed){
-    initializeHillTypes();
-    if (HILL_TYPES.containsKey(hillType))   this.hillType = hillType;
-    else                                    this.hillType = "normal";
+  public SkiJump(Hill hill, double jumpSpeed){
+    this.hill = hill;
     this.jumpSpeed = jumpSpeed;
-  }
-
-  private void initializeHillTypes() {
-    HILL_TYPES.put("normal", new double[]{46.0, 2.0, 90.0});
-    HILL_TYPES.put("large", new double[]{70.0, 1.8, 120.0});
   }
   
   public double calculatePoints(){
-    double height = HILL_TYPES.get(hillType)[0];
-    double pointsPerMeter = HILL_TYPES.get(hillType)[1];
-    double par = HILL_TYPES.get(hillType)[2];
-    
-    return 60 + ((jumpSpeed * Math.sqrt((2 * height) / 9.8) - par) * pointsPerMeter);
+    return 60 + ((jumpSpeed * Math.sqrt((2 * hill.getHeight()) / 9.8) - hill.getPar()) * hill.getPointsPerMeter());
   }
 
   public String getResult(){
     double points = calculatePoints();
-
     if (points >= 61)      return "Great job for doing better than par!";
     else if (points < 10)  return "What happened?";
     else                   return "Sorry you didn’t go very far.";
