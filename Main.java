38a442d86b7ee57f0ffd575b631da50c823ca2fc
cyrusmccollim <106127824+cyrusmccollim@users.noreply.github.com>
2024-02-01 15:15:23 -0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -4,9 +4,8 @@ public class Main {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         
-        System.out.println("Enter the hill details. \n(Format: height pointsPerMeter par)");
-        
         double[] hillDetails = Hill.inputHillDetails();
+
         Hill hill = new Hill(hillDetails);
 
         System.out.print("Enter the jumper's speed: ");
