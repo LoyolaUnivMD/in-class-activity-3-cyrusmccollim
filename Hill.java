0a394b4ae470676df995216704ec2172e7640d6e
cyrusmcco <mccollimcyrus@gmail.com>
2024-02-02 01:39:11 -0500

    Refactor

--- Hill.java
+++ Hill.java
@@ -1,40 +1,47 @@
 import java.util.*;
 
 public class Hill {
-  private final double[] hillDetails; // Format: [height, pointsPerMeter, par]
+  public static HashMap<String, Hill> HILLS = new HashMap<>();
+  private final double height, pointsPerMeter, par;
 
-  public Hill(double[] hillDetails) { this.hillDetails = hillDetails; }
+  public static String inputValidHillType(){
+    Scanner input = new Scanner(System.in);
+
+    System.out.print("\tEnter response: ");
+    String hillType = input.nextLine().strip().toLowerCase();
+
+    while (!HILLS.containsKey(hillType)){
+      System.out.print("\tInvalid entry. Try again: ");
+      hillType = input.nextLine().strip().toLowerCase();
+    }
+
+    return hillType;
+  }
+
+  public static List<String> getHillTypes(){
+    List<String> hillTypes = new ArrayList<>();
+    for (Map.Entry<String, Hill> hill : HILLS.entrySet()){
+      hillTypes.add(hill.getKey());
+    }
+    return hillTypes;
+  }
+
+  public Hill(String name, double height, double pointsPerMeter, double par) {
+    this.height = height;
+    this.pointsPerMeter = pointsPerMeter;
+    this.par = par;
+    HILLS.put(name, this);
+  }
 
   public double getHeight(){
-    return hillDetails[0];
+    return height;
   }
 
   public double getPointsPerMeter(){
-    return hillDetails[1];
+    return pointsPerMeter;
   }
 
   public double getPar(){
-    return hillDetails[2];
-  }
-
-  public static double[] inputHillDetails(){
-
-    double[] details = new double[]{46.0, 2.0, 90.0}; // The default hill details, called "normal".
-
-    System.out.print("\n[Format: height pointsPerMeter par]\nEnter the hill details: ");
-    Scanner input = new Scanner(System.in);
-    String[] hillDetailsInput = input.nextLine().split(" ");
-        
-    try {
-      double[] detailsTemp = new double[3];
-      for (int i = 0; i < 3; i++){
-        detailsTemp[i] = Double.parseDouble(hillDetailsInput[i]);
-      }
-      details = detailsTemp;
-    } catch (Exception e) {
-      System.out.println("An error occurred, hill details set to \"normal\".");
-    }
-
-    return details;
+    return par;
   }
 }
--- SkiJump.java
+++ SkiJump.java
@@ -1,5 +1,3 @@
-import java.util.*;
-
 public class SkiJump {
   private final Hill hill;
   private final double jumpSpeed;
@@ -13,7 +11,7 @@ public class SkiJump {
     return 60 + ((jumpSpeed * Math.sqrt((2 * hill.getHeight()) / 9.8) - hill.getPar()) * hill.getPointsPerMeter());
   }
 
-  public String getResult(){
+  public String result(){
     String result = "\nThe results are in. ";
 
     double points = calculatePoints();
@@ -22,19 +20,5 @@ public class SkiJump {
     else                   result += "Sorry, you did not go very far.";
 
     return result;
-  }
-
-  public static double inputJumperSpeed(){
-    double jumpSpeed = 25.0; // The average.
-
-    try {
-      System.out.print("\nEnter the jumper's speed: ");
-      Scanner input = new Scanner(System.in);
-      jumpSpeed = input.nextDouble();
-    } catch (Exception e){
-      System.out.println("An error occurred, jump speed set to the average.");
-    }
-
-    return jumpSpeed;
   }
 }
